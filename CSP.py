ba3ab76f69a7b156bc0cc69bfb9c347eb622c6b2
Yash Shetty <yashshetty@Yashs-MacBook-Pro.local>
2019-04-04 18:23:05 -0400

    fixed bugs in CSP: initializeDomains. Psuedocode for backtracking search done

--- CSP.py
+++ CSP.py
@@ -1,7 +1,7 @@
 class CSP:
 	def __init__(self, grid_size, discConstant):
 		self.variables = self.initializeVariables(grid_size)
-		self.domain = self.initializeDomain(grid_size)
+		self.domains = self.initializeDomain(grid_size)
 		self.constraints = self.checkConstraints()
 		self.discConstant = 1
 
@@ -9,7 +9,7 @@ class CSP:
 	def initializeVariables(self, grid_size):
 		temp = []
 		for i in range(grid_size):
-			beam = "beam" + i
+			beam = "beam" + str(i)
 			temp.append(beam)
 		return temp
 
@@ -17,38 +17,45 @@ class CSP:
 		domains = {}
 		startPos = (0,0)
 		for beam in self.variables:
-			domains[beam] = [[]]
+			domains[beam] = []
 
 		#array for beam0
 		array1 = []
 		for i in range(grid_size):
-			(startPos, (i,grid_size-1))
+			array1.append((startPos, (i,grid_size-1)))
 		domains["beam0"] = [array1]
 		
 		curBeam = 1
 		top = False
-		while curBeam < grid_size: 
-			oldbeam = "beam" + (curBeam -1)
-			newbeam = "beam" + curBeam
+		while curBeam < grid_size:
+			oldbeam = "beam" + str((curBeam -1))
+			newbeam = "beam" + str(curBeam)
 			prevMapValue =  domains[oldbeam]
 			newMapValue = domains[newbeam]
 
 			startPositions = []
-			for tup in prevMapValue:
-				startPositions.append(tup[1])
+			for domainList in prevMapValue:
+				for tup in domainList:
+					startPositions.append(tup[1])
 
 			for startPosition in startPositions:
 				temp = []
 				if top:
 					for i in range(grid_size):
-						(startPosition, (i, grid_size-1))
+						temp.append((startPosition, (i, grid_size-1)))
 				else:
 					for i in range(grid_size):
-						(startPosition, (i, 0))
+						temp.append((startPosition, (i, 0)))
+				newMapValue.append(temp)
 			top = not top
 			curBeam+=1
 		return domains
 
-	def checkConstraints()
+	def checkConstraints(self):
+		return
+		#do nothing
 
+
+
+csp = CSP(5, 0)
 
---
+++ qLearning.py
@@ -0,0 +1,73 @@
+class QLearningAgent(ReinforcementAgent):
+    """
+      Q-Learning Agent
+
+      Functions you should fill in:
+        - computeValueFromQValues
+        - computeActionFromQValues
+        - getQValue
+        - getAction
+        - update
+
+      Instance variables you have access to
+        - self.epsilon (exploration prob)
+        - self.alpha (learning rate)
+        - self.discount (discount rate)
+
+      Functions you should use
+        - self.getLegalActions(state)
+          which returns legal actions for a state
+    """
+    def __init__(self, ):
+    
+    def getQValue(self, state, action):
+        """
+          Returns Q(state,action)
+          Should return 0.0 if we have never seen a state
+          or the Q node value otherwise
+        """
+
+    def computeValueFromQValues(self, state):
+        """
+          Returns max_action Q(state,action)
+          where the max is over legal actions.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return a value of 0.0.
+        """
+
+    def computeActionFromQValues(self, state):
+        """
+          Compute the best action to take in a state.  Note that if there
+          are no legal actions, which is the case at the terminal state,
+          you should return None.
+        """
+
+
+    def getAction(self, state):
+        """
+          Compute the action to take in the current state.  With
+          probability self.epsilon, we should take a random action and
+          take the best policy action otherwise.  Note that if there are
+          no legal actions, which is the case at the terminal state, you
+          should choose None as the action.
+
+          HINT: You might want to use util.flipCoin(prob)
+          HINT: To pick randomly from a list, use random.choice(list)
+        """
+        # Pick Action
+
+    def update(self, state, action, nextState, reward):
+        """
+          The parent class calls this to observe a
+          state = action => nextState and reward transition.
+          You should do your Q-Value update here
+
+          NOTE: You should never call this function,
+          it will be called on your behalf
+        """
+
+    def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+    def getValue(self, state):
+        return self.computeValueFromQValues(state)
