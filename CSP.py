cb3bc4364acb52e8dae43bc9b7fce81af20d329a
Yash Shetty <yashshetty@Yashs-MacBook-Pro.local>
2019-04-07 13:25:03 -0400

    added constraint check to make sure no two beams are the same. Persistent singular matrix error means that the CSP is not able to spit out a full assignment.

--- CSP.py
+++ CSP.py
@@ -2,62 +2,66 @@ from Force import Force
 from System import System
 
 class CSP:
-	def __init__(self, grid_size, discConstant):
-            self.variables = self.initializeVariables(grid_size)
-            self.domains = self.initializeDomain(grid_size)
-            self.constraints = self.checkConstraints
-            self.discConstant = 1
+    def __init__(self, grid_size, discConstant):
+        self.variables = self.initializeVariables(grid_size)
+        self.domains = self.initializeDomain(grid_size)
+        self.constraints = self.checkConstraints
+        self.discConstant = 1
 
+    def initializeVariables(self, grid_size):
+        temp = []
+        for i in range(grid_size):
+            beam = "beam" + str(i)
+            temp.append(beam)
+        return temp
 
-	def initializeVariables(self, grid_size):
-            temp = []
-            for i in range(grid_size):
-                beam = "beam" + str(i)
-                temp.append(beam)
-            return temp
+    def initializeDomain(self, grid_size):
+        domains = {}
+        startPos = (0,0)
+        for beam in self.variables:
+            domains[beam] = []
 
-	def initializeDomain(self, grid_size):
-            domains = {}
-            startPos = (0,0)
-            for beam in self.variables:
-                domains[beam] = []
+        #array for beam0
+        array1 = []
+        for i in range(grid_size):
+            array1.append((startPos, (i,grid_size-1)))
+        domains["beam0"] = [array1]
 
-            #array for beam0
-            array1 = []
-            for i in range(grid_size):
-                array1.append((startPos, (i,grid_size-1)))
-            domains["beam0"] = [array1]
+        curBeam = 1
+        top = False
+        while curBeam < grid_size:
+            oldbeam = "beam" + str((curBeam -1))
+            newbeam = "beam" + str(curBeam)
+            prevMapValue =  domains[oldbeam]
+            newMapValue = domains[newbeam]
 
-            curBeam = 1
-            top = False
-            while curBeam < grid_size:
-                oldbeam = "beam" + str((curBeam -1))
-                newbeam = "beam" + str(curBeam)
-                prevMapValue =  domains[oldbeam]
-                newMapValue = domains[newbeam]
+            startPositions = []
+            for domainList in prevMapValue:
+                for tup in domainList:
+                    startPositions.append(tup[1])
 
-                startPositions = []
-                for domainList in prevMapValue:
-                    for tup in domainList:
-                        startPositions.append(tup[1])
+            for startPosition in startPositions:
+                temp = []
+                if top:
+                    for i in range(grid_size):
+                        temp.append((startPosition, (i, grid_size-1)))
+                else:
+                    for i in range(grid_size):
+                        temp.append((startPosition, (i, 0)))
+                newMapValue.append(temp)
+            top = not top
+            curBeam+=1
+        return domains
 
-                for startPosition in startPositions:
-                    temp = []
-                    if top:
-                        for i in range(grid_size):
-                            temp.append((startPosition, (i, grid_size-1)))
-                    else:
-                        for i in range(grid_size):
-                            temp.append((startPosition, (i, 0)))
-                    newMapValue.append(temp)
-                top = not top
-                curBeam+=1
-            return domains
+    def checkConstraints(self, assignment):
+        print("assignment:{}".format(assignment))
+        #code to make sure no to beams are the same.
+        assignmentTups = []
+        for key in assignment:
+            tup = assignment[key]
+            assignmentTups.append(tup)
 
-
-	
-
-	def checkConstraints(self, assignment):
+        if self.noSameBeams(assignmentTups):
             nodes = []
             for key in assignment:
                 nodeA, nodeB = assignment[key]
@@ -95,8 +99,21 @@ class CSP:
             thresholdDisplacement = 0.01
             validAssignment = not(abs(max(solutions)) > thresholdDisplacement or abs(min(solutions)) > thresholdDisplacement)
             return validAssignment
+        else:
+            return False
 
-
+    def noSameBeams(self, assignmentTups):
+        i = 0
+        while i < len(assignmentTups):
+            cur = assignmentTups[i]
+            for j in range(i+1, len(assignmentTups)):
+                if assignmentTups[j][0] == cur[1] and assignmentTups[j][1] == cur[0]:
+                    return False
+                if assignmentTups[j] == cur:
+                    return False
+            i+=1
+        return True
+ 
 
 
 csp = CSP(5, 0)
