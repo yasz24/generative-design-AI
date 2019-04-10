aa2a24c97c0cb6517b1cce3e91d9f44dac5aff0d
Anthony <rogers.a@husky.neu.edu>
2019-04-09 23:51:53 -0400

    Method stubs from regression

--- regression.py
+++ regression.py
@@ -1,14 +1,25 @@
 import numpy as np
+import featureExtractor
 
-def evaluate(theta, x):
-    return sum([theta[i] * x[i] for i in range(len(x))])
+class Regression:
+    def __init__(self):
+        pass
 
-def lr(features, targets, sq =False, N = 1000, alpha = 0.01):
-    theta = [0]*len(features[0])
-    for k in range(N):
-        i = k % len(features)
-        h = evaluate(theta, features[i])
-        error = targets[i] - h
-        for j in range(len(theta)):
-            theta[j] += alpha * error * features[i][j]
-    return theta
+    def featureMatrix(self,dataSet):
+        pass
+
+    def targetMatrix(self, dataset):
+        pass 
+
+    def evaluate(self, theta, x):
+        return sum([theta[i] * x[i] for i in range(len(x))])
+
+    def lr(self, features, targets, N = 1000, alpha = 0.01):
+        theta = [0]*len(features[0])
+        for k in range(N):
+            i = k % len(features)
+            h = evaluate(theta, features[i])
+            error = targets[i] - h
+            for j in range(len(theta)):
+                theta[j] += alpha * error * features[i][j]
+        return theta
