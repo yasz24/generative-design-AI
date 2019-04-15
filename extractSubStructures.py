bd8ee751f2ceaacd3fdf9d89a38753dc38030c15
Yash Shetty <yashshetty@Yashs-MacBook-Pro.local>
2019-04-15 00:48:43 -0400

    completed code for sub-structure extractions, needs debugging

--- extractSubStructures.py
+++ extractSubStructures.py
@@ -1,3 +1,5 @@
+from util import *
+
 class ExtractSubStructures:
 	def constructGraph(self, assignment):
 		graph = {}
@@ -9,8 +11,8 @@ class ExtractSubStructures:
 				graph[tuple(structure[key][1])] = []
 
 		for key in structure:
-			graph[tuple(structure[key][0])].append([tuple(structure[key][0]), tuple(structure[key][1])])
-			graph[tuple(structure[key][1])].append([tuple(structure[key][0]), tuple(structure[key][1])])
+			graph[tuple(structure[key][0])].append((tuple(structure[key][0]), tuple(structure[key][1])))
+			graph[tuple(structure[key][1])].append((tuple(structure[key][0]), tuple(structure[key][1])))
 
 		return graph
 
@@ -20,14 +22,14 @@ class ExtractSubStructures:
 		subStructures = {}
 
 		for vertex in graph:
-			initialNode = Node(vertex, [], 0)
-			frontierStates = []
-			frontier = Stack()
-			frontier.push(initialNode)
-			frontierStates.append(vertex)
-			explored = {}
-
+			print("vertex")
 			for j in range(3, len(assignment) + 1):
+				initialNode = Node(vertex, [], 0)
+				frontierStates = []
+				frontier = Stack()
+				frontier.push(initialNode)
+				frontierStates.append(vertex)
+				explored = {}
 				while True:
 					if frontier.isEmpty():
 						break
@@ -35,54 +37,33 @@ class ExtractSubStructures:
 					cur_node = frontier.pop()
 					frontierStates.pop()
 					explored[cur_node.state] = True
+					if cur_node.cost > j :
+						continue
 					if vertex == cur_node.state and cur_node.cost == j:
 						if j in subStructures:
 							#need to remove duplicates
 							#write make structure
-							subStructures[j].append(self.makeStructure(cur_node.path))
+							subStructures[j].append(cur_node.path)
 						else:
 							subStructures[j] = []
-							subStructures[j].append(self.makeStructure(cur_node.path))
+							subStructures[j].append(cur_node.path)
 						continue
-					for edge in problem.getSuccessors(cur_node.state):
-						next_state, action, cost = successor
+					for edge in graph[vertex]:
+						if edge[0] == cur_node.state:
+							next_state = edge[1]
+						else:
+							next_state = edge[0]
+						cost = cur_node.cost+1
 						if not (next_state in explored):
 							updatePath = []
 							updatePath.extend(cur_node.path)
-							updatePath.append(action)
+							updatePath.append(edge)
 							next_node = Node(next_state, updatePath, cost)
 							frontier.push(next_node)
 							frontierStates.append(next_state)
+		print("here")
+		return subStructures
 
 
-
-
-
-class Node:
-    def __init__(self, state, path, cost):
-        self.state = state
-        self.path = path
-        self.cost = cost
-
-    def __eq__(self, other):
-        return self.state == other.state
-
-
-class Stack:
-    "A container with a last-in-first-out (LIFO) queuing policy."
-    def __init__(self):
-        self.list = []
-
-    def push(self,item):
-        "Push 'item' onto the stack"
-        self.list.append(item)
-
-    def pop(self):
-        "Pop the most recently pushed item from the stack"
-        return self.list.pop()
-
-    def isEmpty(self):
-        "Returns true if the stack is empty"
-        return len(self.list) == 0
-
-
+structure = {'beam0': ((0, 0), (1, 2)), 'beam1': ((1, 2), (2, 2)), 'beam2': ((2, 2), (3, 2)),'beam3': ((3, 2), (0, 0)), 'beam4': ((0, 0), (2, 2)), 'beam5': ((2, 2), (0, 3)), 'beam6': ((0, 3), (3, 2)), 'beam7': ((3, 2), (1, 2)), 'beam8': ((1, 2), (0, 3)), 'beam9': ((0, 3), (0, 0))}
+print(ExtractSubStructures().extractSubStructures(structure))
