d7b2ef942ce6ab6bc242d9329d43550d3172ba74
Anthony <rogers.a@husky.neu.edu>
2019-04-15 13:35:59 -0400

    Trying an idea with a minimum spanning tree

--- substructureExtractor.py
+++ substructureExtractor.py
@@ -1,9 +1,14 @@
 import copy
+import numpy as np
+from graphics import StructureVisual
+import itertools
 
 ##structure -> list of structures
 def extractSubstructures(structure):
     substructures = []
     graph = structureToGraph(structure)
+    print(graph)
+    spanningTree = GraphToSpanningTree(graph)
     exploredSet = []
     fringe = [(list(graph.keys())[0], [])]
     while len(fringe) > 0:
@@ -39,5 +44,34 @@ def structureToGraph(structure):
             graph[nodeB] += [nodeA]
     return graph
 
+def GraphToSpanningTree(graph):
+    spanningTree = copy.deepcopy(graph)
+    cyclesExist = True
+    while cyclesExist:
+        for key in spanningTree:
+            removeCycle(spanningTree, key, key)
+        for i in spanningTree:
+            children = spanningTree[i]
+            childrenConnections = itertools.combinations(children, 2)
+            for connection in childrenConnections:
+                if connection[1] in spanningTree[connection[0]]:
+                    spanningTree[connection[0]]. remove(connection[1])
+        cyclesExist = False
+    print(spanningTree)
+    return spanningTree
+
+def removeCycle(spanningTree, key, start):
+
+    if (start in spanningTree[key]):
+        spanningTree[key].remove(start)
+
+        return
+    else:
+        for node in spanningTree[key]:
+            removeCycle(spanningTree, node, start)
+
+
+def getLength(nodeA, nodeB):
+    return np.sqrt((nodeA[0]-nodeB[0])**2 + (nodeA[1]-nodeB[1])**2)
 
 print (extractSubstructures({'beam0': ((0, 0), (0, 3)), 'beam1': ((0, 3), (1, 3)), 'beam2': ((1, 3), (2, 3)), 'beam3': ((2, 3), (0, 0)), 'beam4': ((0, 0), (1, 3))}))
