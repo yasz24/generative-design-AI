b996f80b15992074f4e1d7d23f59a9c5131499c7
Yash Shetty <yashshetty@Yashs-MacBook-Pro.local>
2019-04-15 21:09:11 -0400

    substructure extraction works for size 3

--- extractSubStructures.py
+++ extractSubStructures.py
@@ -22,7 +22,6 @@ class ExtractSubStructures:
 		subStructures = {}
 
 		for vertex in graph:
-			print("vertex")
 			for j in range(3, len(assignment) + 1):
 				initialNode = Node(vertex, [], 0)
 				frontierStates = []
@@ -37,9 +36,15 @@ class ExtractSubStructures:
 					cur_node = frontier.pop()
 					frontierStates.pop()
 					explored[cur_node.state] = True
+					print("j = {}".format(j))
+					print("cur_node cost = {}".format(cur_node.cost))
 					if cur_node.cost > j :
 						continue
+					print("vertex {}".format(vertex))
+					print("cur_node.state {}".format(cur_node.state))
+					print(vertex == cur_node.state)
 					if vertex == cur_node.state and cur_node.cost == j:
+						print("append to subStructures")
 						if j in subStructures:
 							#need to remove duplicates
 							#write make structure
@@ -48,22 +53,24 @@ class ExtractSubStructures:
 							subStructures[j] = []
 							subStructures[j].append(cur_node.path)
 						continue
-					for edge in graph[vertex]:
+					for edge in graph[cur_node.state]:
 						if edge[0] == cur_node.state:
 							next_state = edge[1]
 						else:
 							next_state = edge[0]
 						cost = cur_node.cost+1
-						if not (next_state in explored):
+						if not (next_state in explored) or next_state == vertex:
 							updatePath = []
 							updatePath.extend(cur_node.path)
 							updatePath.append(edge)
 							next_node = Node(next_state, updatePath, cost)
+							print("new node--state:{}, path:{}, cost:{}".format(next_node.state, next_node.path, next_node.cost))
 							frontier.push(next_node)
 							frontierStates.append(next_state)
 		print("here")
 		return subStructures
 
 
-structure = {'beam0': ((0, 0), (1, 2)), 'beam1': ((1, 2), (2, 2)), 'beam2': ((2, 2), (3, 2)),'beam3': ((3, 2), (0, 0)), 'beam4': ((0, 0), (2, 2)), 'beam5': ((2, 2), (0, 3)), 'beam6': ((0, 3), (3, 2)), 'beam7': ((3, 2), (1, 2)), 'beam8': ((1, 2), (0, 3)), 'beam9': ((0, 3), (0, 0))}
+structure = {"beam0": [[0, 0], [4, 0]], "beam1": [[4, 0], [4, 4]], "beam2": [[4, 4], [0, 4]], "beam3": [[0, 4], [0, 0]], "beam4": [[0, 4], [4, 0]], "beam5": [[0,0], [4,0]]}
+
 print(ExtractSubStructures().extractSubStructures(structure))
