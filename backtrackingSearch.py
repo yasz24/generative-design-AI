e6163c356b2f7af1fd7e4a8644cfb76b424f37f8
Yash Shetty <yashshetty@Yashs-MacBook-Pro.local>
2019-04-20 22:25:49 -0400

    refactored backtracking search to a obj-oriented class

--- backtrackingSearch.py
+++ backtrackingSearch.py
@@ -3,160 +3,164 @@ from CSP import *
 from random import shuffle
 from graphics import *
 from featureExtractor import *
-#from regression import *
+from regression import *
 import json
 import csv
 from util import *
 
 import time
 start = time.time()
-"the code you want to test stays here"
 
-with open('Weights.csv', 'r') as f:
-    reader = csv.reader(f)
-    weights = list(reader)
+class BacktrackingSearch:
+    def __init__(self):
+        with open('Weights.csv', 'r') as f:
+            reader = csv.reader(f)
+            weights = list(reader)
+        weights = weights[0]
+        for i in range(len(weights)):
+            weights[i] = float(weights[i])
+        self.weights = weights
+        print(self.weights)
 
-weights = weights[0]
-for i in range(len(weights)):
-    weights[i] = float(weights[i])
-print(weights)
-
-def backtrackingSearch(csp):
-    return recursiveBackTrackingSearch({}, csp.variables, csp.domains, csp.constraints)
-
-
-def recursiveBackTrackingSearch(assignment, variables, domains, constraints):
-    if assignmentComplete(assignment, variables):
-        return assignment
-    next_var = chooseVariable(assignment, variables)
-	#best approach probably to merge items in each domain list.
-    for domainList in domains[next_var]:
-        #could do an argmax here to make the best possible assignment/ q-learning here
-        """Some sort of regression choosing should go here"""
-        """*************************************"""
-       #a = json.dumps(assignment)
-       #hypothesis = evaluate(weights, FeatureExtractorUtil().extractFeatures(a))
-        """*************************************"""
-        #can also randomize values picked here, as well as the domainList picked.
-        domainList = list(domainList)
-        shuffle(domainList)
-        for next_val in domainList:
-            assignment[next_var] = next_val
-            print("****************next_val********************")
-            print(next_val)
-            old_domains = createDeepCopy(domains)
-            if validAssignment(assignment, constraints):
-                print("*********validAssignment next_val{}".format(next_val))
-                #print("next_val{}".format(next_val))
-                updateDomains(assignment, variables, domains, next_val)
-                if noEmptyDomain(domains):
-                    result = recursiveBackTrackingSearch(assignment, variables, domains, constraints)
-                    if result is not None:
-                        return result
-            del assignment[next_var]
-            print("****backtracked next_val{}, next_var{}".format(next_val, next_var))
-
-            domains = old_domains
-    return None
+    def backtrackingSearch(self, csp):
+        return self.recursiveBackTrackingSearch({}, csp.variables, csp.domains, csp.constraints)
 
 
+    def recursiveBackTrackingSearch(self, assignment, variables, domains, constraints):
 
-def backTrackingSearchWithHeuristic(assignment, variables, domains, constraints):
-    if assignmentComplete(assignment, variables):
-        return assignment
-    next_var = chooseVariable(assignment, variables)
-    domain = PriorityQueue()
-    for domainList in domains[next_var]:
-        for domain in domainList:
-            #create a priority queue where priorities are the hypothesis computed using the weights.
-            #pop items in order of lowest priority first
+        if self.assignmentComplete(assignment, variables):
+            return assignment
+        next_var = self.chooseVariable(assignment, variables)
+    	#best approach probably to merge items in each domain list.
+        for domainList in domains[next_var]:
+            #could do an argmax here to make the best possible assignment/ q-learning here
+            """Some sort of regression choosing should go here"""
+            """*************************************"""
+           #a = json.dumps(assignment)
+           #hypothesis = evaluate(weights, FeatureExtractorUtil().extractFeatures(a))
+            """*************************************"""
+            #can also randomize values picked here, as well as the domainList picked.
+            domainList = list(domainList)
+            shuffle(domainList)
+            for next_val in domainList:
+                assignment[next_var] = next_val
+                print("****************next_val********************")
+                print(next_val)
+                old_domains = self.createDeepCopy(domains)
+                if self.validAssignment(assignment, constraints):
+                    print("*********validAssignment next_val{}".format(next_val))
+                    #print("next_val{}".format(next_val))
+                    self.updateDomains(assignment, variables, domains, next_val)
+                    if self.noEmptyDomain(domains):
+                        result = self.recursiveBackTrackingSearch(assignment, variables, domains, constraints)
+                        if result is not None:
+                            return result
+                del assignment[next_var]
+                print("****backtracked next_val{}, next_var{}".format(next_val, next_var))
 
-    #best approach probably to merge items in each domain list.
-    for domainList in domains[next_var]:
-        #could do an argmax here to make the best possible assignment/ q-learning here
-        """Some sort of regression choosing should go here"""
-        """*************************************"""
-       #a = json.dumps(assignment)
-       #hypothesis = evaluate(weights, FeatureExtractorUtil().extractFeatures(a))
-        """*************************************"""
-        #can also randomize values picked here, as well as the domainList picked.
-        domainList = list(domainList)
-        shuffle(domainList)
-        for next_val in domainList:
-            assignment[next_var] = next_val
-            print("****************next_val********************")
-            print(next_val)
-            old_domains = createDeepCopy(domains)
-            if validAssignment(assignment, constraints):
-                print("*********validAssignment next_val{}".format(next_val))
-                #print("next_val{}".format(next_val))
-                updateDomains(assignment, variables, domains, next_val)
-                if noEmptyDomain(domains):
-                    result = recursiveBackTrackingSearch(assignment, variables, domains, constraints)
-                    if result is not None:
-                        return result
-            del assignment[next_var]
-            print("****backtracked next_val{}, next_var{}".format(next_val, next_var))
-
-            domains = old_domains
-    return None
-
-def assignmentComplete(assignment, variables):
-    for var in variables:
-        if var not in assignment:
-            return False
-    return True
+                domains = old_domains
+        return None
 
 
-def chooseVariable(assignment, variables):
-    for var in variables:
-        if var not in assignment:
-            return var
-    return None
 
-
-def validAssignment(assignment, constraints):
-    return constraints(assignment)
-
-
-def updateDomains(assignment, variables, domains, next_val):
-    #print("********domains{}".format(domains))
-    var_for_domain_update = chooseVariable(assignment, variables)
-    if var_for_domain_update == None:
-        return
-    # print("********var_for_domain_update{}".format(var_for_domain_update))
-    # print("********next_val{}".format(next_val))
-    domainMapVal = domains[var_for_domain_update]
-    newDomainMapVal = []
-    for domainList in domainMapVal:
-
-        if domainList[0][0] == next_val[1]:
-            newDomainMapVal.append(domainList)
-
-    domains[var_for_domain_update] = newDomainMapVal
-    return
-
-def noEmptyDomain(domains):
-    #print(domains)
-    for key in domains:
-        if len(domains[key]) == 0:
-            # print("returning False")
-            return False
-    return True
-
-def createDeepCopy(domains):
-    newDomain = {}
-    for key in domains:
-        newValue = []
-        value = domains[key]
-        for domainList in value:
-            newDomainList = []
+    def backTrackingSearchWithHeuristic(assignment, variables, domains, constraints):
+        if self.assignmentComplete(assignment, variables):
+            return assignment
+        next_var = self.chooseVariable(assignment, variables)
+        domain = PriorityQueue()
+        for domainList in domains[next_var]:
             for domain in domainList:
-                newDomainList.append(domain)
-            newValue.append(newDomainList)
-        newDomain[key] = newValue
-    return newDomain
+                return
+                #create a priority queue where priorities are the hypothesis computed using the weights.
+                #pop items in order of lowest priority first 
 
+        #best approach probably to merge items in each domain list.
+        for domainList in domains[next_var]:
+            #could do an argmax here to make the best possible assignment/ q-learning here
+            """Some sort of regression choosing should go here"""
+            """*************************************"""
+           #a = json.dumps(assignment)
+           #hypothesis = evaluate(weights, FeatureExtractorUtil().extractFeatures(a))
+            """*************************************"""
+            #can also randomize values picked here, as well as the domainList picked.
+            domainList = list(domainList)
+            shuffle(domainList)
+            for next_val in domainList:
+                assignment[next_var] = next_val
+                print("****************next_val********************")
+                print(next_val)
+                old_domains = self.createDeepCopy(domains)
+                if self.validAssignment(assignment, constraints):
+                    print("*********validAssignment next_val{}".format(next_val))
+                    #print("next_val{}".format(next_val))
+                    self.updateDomains(assignment, variables, domains, next_val)
+                    if self.noEmptyDomain(domains):
+                        result = self.recursiveBackTrackingSearch(assignment, variables, domains, constraints)
+                        if result is not None:
+                            return result
+                del assignment[next_var]
+                print("****backtracked next_val{}, next_var{}".format(next_val, next_var))
+
+                domains = old_domains
+        return None
+
+    def assignmentComplete(self, assignment, variables):
+        for var in variables:
+            if var not in assignment:
+                return False
+        return True
+
+
+    def chooseVariable(self, assignment, variables):
+        for var in variables:
+            if var not in assignment:
+                return var
+        return None
+
+
+    def validAssignment(self, assignment, constraints):
+        return constraints(assignment)
+
+
+    def updateDomains(self, assignment, variables, domains, next_val):
+        #print("********domains{}".format(domains))
+        var_for_domain_update = self.chooseVariable(assignment, variables)
+        if var_for_domain_update == None:
+            return
+        # print("********var_for_domain_update{}".format(var_for_domain_update))
+        # print("********next_val{}".format(next_val))
+        domainMapVal = domains[var_for_domain_update]
+        newDomainMapVal = []
+        for domainList in domainMapVal:
+
+            if domainList[0][0] == next_val[1]:
+                newDomainMapVal.append(domainList)
+
+        domains[var_for_domain_update] = newDomainMapVal
+        return
+
+    def noEmptyDomain(self, domains):
+        #print(domains)
+        for key in domains:
+            if len(domains[key]) == 0:
+                # print("returning False")
+                return False
+        return True
+
+    def createDeepCopy(self, domains):
+        newDomain = {}
+        for key in domains:
+            newValue = []
+            value = domains[key]
+            for domainList in value:
+                newDomainList = []
+                for domain in domainList:
+                    newDomainList.append(domain)
+                newValue.append(newDomainList)
+            newDomain[key] = newValue
+        return newDomain
+
+BacktrackingSearch()
 
 
 #csp = CSP(10, 0)
